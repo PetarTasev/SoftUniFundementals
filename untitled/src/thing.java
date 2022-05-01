fa37137ab6109fdc8016ba35374a6de78090e6b0
Petar Stefanov <petar.g.stefanov@gmail.com>
2022-05-01 20:05:25 +0300

    Create thing.java

---
+++ untitled/src/thing.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class thing {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("we made it");
+    }
+}
